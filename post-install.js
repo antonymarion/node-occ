ddb1b776d4dfeac981b4a3b63d1781727d0bd47a
amazon cloud machine <aws@amazon.com>
2020-12-09 18:50:01 +0100

    use generic vOCC var from Standard_Version.hxx header in node-pre-gyp building tools

--- post-install.js
+++ post-install.js
@@ -1,16 +1,19 @@
-
-const { exec }= require("child_process");
+const {exec} = require("child_process");
 
 
 if (process.platform === "linux") {
 
-    const cmd = "export vOCC=$(export VERSION_FILE=/usr/include/opencascade/Standard_Version.hxx && grep -i \"#define OCC_VERSION_COMPLETE\" $VERSION_FILE | awk '{gsub(/\"/, \"\", $0); print $3}') && (cd lib/binding && echo 'linux libs directory' && pwd && ls * && echo $vOCC && ls *.so.$vOCC | sed sP^libPPg | sed sp.so.$vOCCpp | xargs -i ln -sf lib{}.so.$vOCC lib{}.so.7)";
+    const cmd = "export vOCC=$(export VERSION_FILE=/usr/include/opencascade/Standard_Version.hxx && grep -i \"#define OCC_VERSION_COMPLETE\" $VERSION_FILE | awk '{gsub(/\"/, \"\", $0); print $3}') && (cd lib/binding && echo 'linux libs directory' && pwd && ls * && echo $vOCC)";
 
-    exec(cmd, (error, stdout, stderr)  => {
+    // apt-get install libTBB
+    /* "(cd node_modules/node-occ/lib/binding && pwd && ls *.so.$vOCC) &&  ldconfig $pathToBinding && ldd occ.node"*/
+
+    exec(cmd, (error, stdout, stderr) => {
 
         console.log(`stdout: ${stdout}`);
         if (error) {
-            console.log(error.code);
+            console.log(error);
+            console.log("ERROR!!!", error.code);
         } else {
             console.log("done post install rename libs");
         }
