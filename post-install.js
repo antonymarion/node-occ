f340264b217d85312ee4fff8a8afa1a4d281884c
amazon cloud machine <aws@amazon.com>
2020-12-09 09:58:20 +0100

    ls libs in binding dir.

--- post-install.js
+++ post-install.js
@@ -4,7 +4,7 @@ const { exec }= require("child_process");
 
 if (process.platform === "linux") {
 
-    const cmd = "export VERSION_FILE=/usr/include/opencascade/Standard_Version.hxx && (cd lib/binding && echo 'linux libs directory' && pwd && ls *.so.$VERSION_FILE | sed sP^libPPg | sed sp.so.$VERSION_FILEpp | xargs -i ln -sf lib{}.so.$VERSION_FILE lib{}.so.7)";
+    const cmd = "export VERSION_FILE=/usr/include/opencascade/Standard_Version.hxx && (cd lib/binding && echo 'linux libs directory' && pwd && ls * && ls *.so.$VERSION_FILE | sed sP^libPPg | sed sp.so.$VERSION_FILEpp | xargs -i ln -sf lib{}.so.$VERSION_FILE lib{}.so.7)";
 
     exec(cmd, (error, stdout, stderr)  => {
 
